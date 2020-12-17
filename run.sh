4ccafac8bf46a9dabb54a437eec35e4337b66fe2
Juanes Cendales <juanescendales@gmail.com>
2020-12-16 22:00:34 -0500

    Run.sh for AWS

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cd ..
+export DATASET_CHAIRS = "/home/ec2-user/SageMaker/hologan-pytorch/datasets/select/Select"
+export RESULTS_DIR="/home/ec2-user/SageMaker/hologan-pytorch/results"
 
-export DATASET_CELEBA="/celebA/img_align_celeba"
-export RESULTS_DIR="/results/hologan"
-
-python hologan-pytorch/task_launcher.py --epochs=10 --resume=auto --name=hologan_proyect_chair_v1 --trial_id=trial_01 --save_every=10 --lamb=25.0 --dataset=chairs --angles="[0,0,-90,90,0,0]"
+python task_launcher.py --epochs=200 --resume=auto --name=hologan_proyect_chair_v1 --trial_id=trial_01 --save_every=20 --lamb=25.0 --dataset=chairs --angles="[0,0,-90,90,0,0]"
 
